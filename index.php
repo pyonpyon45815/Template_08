2145e54a6732d4c0ac1a598cb34f3f7201c8ca8a
pyonpyon45815 <gingiraharu2@gmail.com>
2021-08-25 01:15:04 +0900

    doudesyou

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
             <?php while (have_posts()): the_post(); ?>
                 <li>
-                <a href="#"><span class="news-list__block">
+                <a href="<?php the_permalink(); ?>"><span class="news-list__block">
                     <?php the_time( "Y/m/d" ); ?>
                 <span class="news-list__border">
                     <?php the_title(); ?>
