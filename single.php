65e7e25a70ab31f343f3675d89e7ecab9ca4c9f4
pyonpyon45815 <gingiraharu2@gmail.com>
2021-08-28 00:46:12 +0900

    iikannji

--- single.php
+++ single.php
@@ -14,7 +14,7 @@
 <body>
     <div class="wrap">
         <!-- ヘッダー -->
-        <div class=""  style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/img/danist-soh-dqXiw7nCb9Q-unsplash.jpg)">
+        <div class="bg-no-repeat bg-cover bg-center h-96 relative opacity-80" style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/img/danist-soh-dqXiw7nCb9Q-unsplash.jpg)">
             <div class="header-container">
                 <header class="header">
                     <!-- ヘッダロゴ -->
@@ -42,7 +42,7 @@
     <h3 class="text-center mt-4 text-7xl text-blue-900 bg-yellow-500 ">HELLO World!!</h3>
     <h4 class="font-bold text-7xl mt-4 text-right text-red-200 bg-green-300">おはこんにちは</h4>
     <button class="button-action text-right mt-5 text-red-600 text-6xl hover:bg-yellow-500">ここ押してみて</button>
-    <h5 class="text-red-600 bg-green-500 text-5xl mt-5 text-center font-bold">これでどうでしょう？</h5>
+    <h5 class="text-red-600 bg-green-300 text-5xl mt-5 text-right">これでどうでしょう？</h5>
 
 
 <?php get_footer(); ?>
