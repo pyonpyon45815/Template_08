27f6f94291ace3f6a892d0600c8672b51337f4fa
pyonpyon45815 <gingiraharu2@gmail.com>
2021-09-03 00:56:19 +0900

    iikannji

--- functions.php
+++ functions.php
@@ -2,7 +2,18 @@
   function init_func() {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
-    set_post_thumbnail_size(600,600,true);
+
+    register_post_type('item', [
+      'labels' => [
+        'name' => '商品',
+      ],
+      'public' => true,
+      'has_archive' => true,
+      'hierarchical' => false,
+      'menu_position' => 5,
+      'menu_icon' => ''
+
+    ]);
   }
   add_action('init', 'init_func');
 
