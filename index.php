1817d919e26df4e5dcf7afe8ee891d5501a38066
pyonpyon45815 <gingiraharu2@gmail.com>
2021-09-19 15:48:25 +0900

    iikannji

--- index.php
+++ index.php
@@ -1,45 +1,10 @@
-<!DOCTYPE html>
-<html lang="ja">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/reset.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/dist.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <?php wp_head(); ?>
-</head>
-<body>
-    <div class="wrap">
-        <!-- ヘッダー -->
-        <div class="header-wrap">
-            <div class="header-container">
-                <header class="header">
-                    <!-- ヘッダロゴ -->
-                    <h1>
-                        <a href="http://localhost/bluehouseHP/"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/header_logo.png" alt="蒼乃建設" class="header-logo border-none"></a>
-                    </h1>
-                    <!-- ヘッダーナビ -->
-                    <nav class="header-nav">
-                    <?php get_template_part('includes/nav'); ?>
-                    </nav>
-                    <div class="header-icon"><img class="border-none" src="<?php echo get_template_directory_uri(); ?>/assets/img/hamburger.svg" alt="ハンバーガーメニュー"></div>
-                </header>
-                <!-- ファーストビューの文章 -->
-                <p class="header-main__text">街を作る。未来を作る。</p>
-            </div>
-        </div>
-
-
-
-
+<?php get_header(); ?> 
 
 
         <!-- お知らせ -->
         <section class="news">
-            <h2 class="news-title text-red-800 font-bold">お知らせ</h2>
+            <h2 class="news-title">お知らせ</h2>
             <ul class="news-list">
 
             <?php while (have_posts()): the_post(); ?>
@@ -62,7 +27,6 @@
             </ul>
         </section>
 
-        <div class="text-3xl font-bold bg-green-300">テストです。どんなかんじ</div>
     
         <!-- 施工事例 -->
         <section class="case">
@@ -72,7 +36,7 @@
                     <p class="case-content__img"><img class="border-none" src="<?php echo get_template_directory_uri(); ?>/assets/img/building1.png" alt="M様邸新築工事"></p>
                     <h3 class="case-content__title">M様邸新築工事</h3>
                     <p class="case-content__text">埼玉県</p>
-                    <a href="#" class="case-content__btn"><img class="border-none case-content__img" src="<?php echo get_template_directory_uri(); ?>/assets/img/more.png" alt="詳細を見る"></a>
+                    <a href="#" class="case-content__btn"><img class="border-none container mx-auto" src="<?php echo get_template_directory_uri(); ?>/assets/img/more.png" alt="詳細を見る"></a>
                 </section>
 
 
@@ -96,34 +60,6 @@
         </div>
 
 
+        <?php get_footer(); ?> 
 
-
-   <footer class="footer">
-            <div class="footer-content">
-                <div class="footer-nav">
-                    <!-- フッターロゴ -->
-                    <p>
-                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/footer_logo.png" alt="蒼乃建設" class="footer-logo "></a>
-                    </p>
-                    <!-- フッターナビ -->
-                    <nav class="footer-nav__list">
-                        <ul>
-                            <li><a  href="http://localhost/bluehouseHP/company_philosophy/">企業理念</a></li>
-                            <li><a href="http://localhost/bluehouseHP/construction_case/">施工事例</a></li>
-                            <li><a href="http://localhost/bluehouseHP/special/">採用情報</a></li>
-                            <li><a href="http://localhost/bluehouseHP/item/">お問い合わせ</a></li>
-                        </ul>
-                    </nav>
-                </div>
-                <p class="copyright">Copyright © 2020 Aono Corporation.</p>
-            </div>
-            
-        </footer>
-        
-    </div>
-    <!-- wrap -->
-    <script src="<?php echo get_template_directory_uri(); ?>/script.js"></script>
-    <?php wp_footer(); ?>
-</body>
-
-</html>
+  
