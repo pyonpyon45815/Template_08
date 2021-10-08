8ce4e22724287811d4821c02d1a5477c65ecef18
pyonpyon45815 <gingiraharu2@gmail.com>
2021-10-08 21:09:14 +0900

    iikannji

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
     mode: "jit",
     purge: {
         enabled: true,
-        content: ['./single.php', './index.php', './contact.html']
+        content: ['./single.php', './index.php', './contact.html', './contact2.html']
     },
     darkMode: false,
     theme: {
