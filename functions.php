f7584c10b9068e2597d83d738c88f659b387bd54
pyonpyon45815 <gingiraharu2@gmail.com>
2021-10-10 21:56:56 +0900

    iikannji

--- functions.php
+++ functions.php
@@ -48,16 +48,9 @@ register_post_type(
 
 
 <?php
-
 function rjs_styles() {
-
-
 wp_enqueue_style('tailwindcss', get_template_directory_uri() . '/dist.css', array(), filemtime(get_template_directory() .'/dist.css'), 'all');
-
-
 } 
-
-
 add_action( 'wp_enqueue_scripts', 'rjs_styles' );
 
 
